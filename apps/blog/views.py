24d4cb4b4e80ea2585e5ad11806455e1bd3d773a
Markelov A <mrklv001@gmail.com>
2024-03-29 16:43:17 +0300

    Работа с DetailView, форматирование и обработка кириллицы в Slug

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,45 @@
+from django.views.generic import View
 from django.shortcuts import render
+# from .models import Product
+from .models import Post
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+class ProductList(View):
+
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all()
+        context = {'products': products}
+        return render(request, 'base/product_list.html', context)
+
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+
+class Product(View):
+    def get(self, request, *args, **kwargs):
+        product = get_object_or_404(Product, pk=kwargs['pk'])
+        context = {'product': product}
+        return render(request, 'base/products_detail.html', context)
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object.title
+        return context
