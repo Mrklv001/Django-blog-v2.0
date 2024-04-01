aaf2ad192e2ce6b0be3287a969cf210690c27dd4
Markelov A <mrklv001@gmail.com>
2024-04-01 19:07:48 +0300

    Добавил систему регистрации пользователя и стстему для добавления новых публикаций

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 # from .models import Product
 from django.views.generic import ListView, DetailView
 from .models import Post, Category
-
+from django.views.generic import CreateView
+from .forms import PostCreateForm
+from django.views.generic import UpdateView
+from .forms import PostUpdateForm
+from django.contrib.messages.views import SuccessMessageMixin
+from ..services.mixins import AuthorRequiredMixin
 
 class ProductList(View):
 
@@ -65,3 +70,47 @@ class PostFromCategory(ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = f'Записи из категории: {self.category.title}'
         return context
+
+
+class PostCreateView(CreateView):
+    """
+    Представление: создание материалов на сайте
+    """
+    model = Post
+    template_name = 'blog/post_create.html'
+    form_class = PostCreateForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавление статьи на сайт'
+        return context
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.save()
+        return super().form_valid(form)
+
+
+class PostUpdateView(AuthorRequiredMixin, SuccessMessageMixin, UpdateView):
+    """
+    Представление: обновления материала на сайте
+    """
+    model = Post
+    template_name = 'blog/post_update.html'
+    context_object_name = 'post'
+    form_class = PostUpdateForm
+    login_url = 'home'
+    success_message = 'Запись была успешно обновлена!'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Обновление статьи: {self.object.title}'
+        return context
+
+    def form_valid(self, form):
+        # form.instance.updater = self.request.user
+        form.save()
+        return super().form_valid(form)
+
+
+
